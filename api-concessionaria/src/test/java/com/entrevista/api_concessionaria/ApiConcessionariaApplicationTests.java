dd2a230525c1e3ffc04a16e24283a271c7e00021
Marcus Vinicius Silva Lima de Oliveira <marcus.oliveira@nca.ufma.br>
2026-05-07 14:36:57 -0300

    Criação do projeto utilizando o SpringBoot Initializer, na versão 17 do Java e no spring boot 3.5.14.

---
+++ api-concessionaria/src/test/java/com/entrevista/api_concessionaria/ApiConcessionariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrevista.api_concessionaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConcessionariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
